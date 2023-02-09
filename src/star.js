1dd07277c3ca3d481809204e8eadd0f42d90a2dd
孟世博 <897205285@qq.com>
2023-02-09 13:34:36 +0800

    Create star.js

---
+++ src/star.js
@@ -0,0 +1,73 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+
+let page = 1;
+
+const getData = () => {
+  console.log(page);
+  return fetch(
+    `https://api.github.com/users/exposir/starred?page=${page}&per_page=100`,
+    {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        authorization: "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",
+      },
+    }
+  );
+};
+
+const map = {};
+
+const loading = async () => {
+  const data = await getData();
+  const res = await data.json();
+
+  if (res.length === 0) {
+    return false;
+  }
+
+  res.forEach((item) => {
+    if (!map[item.language]) {
+      map[item.language] = [];
+    }
+    map[item.language].push(item);
+  });
+
+  return true;
+};
+
+const read = async () => {
+  const flag = await loading();
+  if (flag) {
+    page++;
+    await read();
+  }
+};
+
+const filePath = path.resolve(__dirname, `../README.md`);
+const docs = ["# Star \n\n"];
+
+const write = () => {
+  for (i in map) {
+    const language = i;
+    const item = map[i];
+
+    docs.push(`## ${language} \n\n`);
+    item.forEach((i) => {
+      docs.push(`- [${i.name}](${i.html_url}) \n`);
+    });
+  }
+  fs.writeFile(filePath, docs.join(""), function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+};
+
+const start = async () => {
+  await read();
+  write();
+};
+
+start();
